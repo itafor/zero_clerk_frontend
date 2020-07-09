3945fc7c9ec870b51c2d997f1ed2aed87aef764c
deji adesoga <done2stay@gmail.com>
2020-07-09 12:32:00 +0100

    Added Paystack

--- src/app/users/subscription-plans/subscription-plans.component.ts
+++ src/app/users/subscription-plans/subscription-plans.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { UsersService } from 'src/app/services/users.service';
 
 
@@ -9,12 +9,29 @@ import { UsersService } from 'src/app/services/users.service';
   styleUrls: ['./subscription-plans.component.css']
 })
 export class SubscriptionPlansComponent implements OnInit {
+  reference = '';
+  title;
 
   constructor(public _activatedRoute:ActivatedRoute,
-    private service: UsersService,) { }
+    private service: UsersService, private router: Router) { }
 
   ngOnInit() {
     let uuid:string = this._activatedRoute.snapshot.params['uuid'];
+    this.reference = `ref-${Math.ceil(Math.random() * 10e13)}`;
+  }
+
+  paymentInit() {
+    console.log('Payment initialized');
+  }
+
+  paymentDone(ref: any) {
+    this.title = 'Payment successfull';
+    this.router.navigate(['plans'])
+    console.log(this.title, ref);
+  }
+
+  paymentCancel() {
+    console.log('payment failed');
   }
 
 }
